688e1803ec8b02b94731752fdfe26314c1f4ab91
hossamsherif <hossamsherif@aucegypt.edu>
2024-02-27 16:35:25 +0200

    editied users.h file

--- userss.h
+++ userss.h
@@ -1,4 +1,8 @@
 #ifndef USERSS_H
 #define USERSS_H
-
+#include<QString>
+extern QString usernames[100];
+extern QString passwords[100];
+extern int ages[100];
+extern int usersCount;
 #endif // USERSS_H
