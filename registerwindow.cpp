c776b7909ee9bc9934124f8d789def45bd940ecf
hossamsherif <hossamsherif@aucegypt.edu>
2024-03-03 00:46:09 +0200

    test register

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -22,68 +22,50 @@ RegisterWindow::~RegisterWindow()
 
 void RegisterWindow::on_reg_reg_clicked()
 {
-    bool found = false;
-    QString str=ui -> month ->currentText();
-    QString f=ui->day->text();
-    QString r=ui->year->text();
-    QString username1 =( ui->register_username ->text());
-    QString pass1=(ui->register_pass->text());
-    QString pass2=(ui->register_pass_2->text());
-    //int month = str.toInt();
-    //int day=f.toInt();
-    int year=r.toInt();
-    bool acctype=ui->groupBox_acctype->isChecked();
-    bool genre=ui->genre->isChecked();
-    bool gender = ui ->groupBox_gender->isChecked();
-    if(acctype || genre || gender)
-    {
+    QString username1 = ui->register_username->text();
+    QString pass1 = ui->register_pass->text();
+    QString pass2 = ui->register_pass_2->text();
+    QString month = ui->month->currentText();
+    QString day = ui->day->text();
+    QString year = ui->year->text();
+    bool acctype = ui->groupBox_acctype->isChecked();
+    bool genre = ui->genre->isChecked();
+    bool gender = ui->groupBox_gender->isChecked();
 
-    }
-    else
-    {
-        ui -> register_status ->setVisible(true);
-        ui->register_status->setText("ks");
-    }
-
-    if ((2024 - year) < 12)
-    {
-        ui -> year_status -> setVisible(true);
-        ui -> year_status -> setText("Under 12 Years Old");
-    }
-
-    for (int i = 0; i < 100 ; i++)
-    {
-        if (username1 != usernames[i])
-        {
-
-        }
-        else
-        {
-            found = true;
-            break;
-        }
-    }
-    if (found)
-    {
-        ui -> username_stat -> setVisible(true);
-        ui -> username_stat -> setText("Username already exists");
-
-    }
-
-    if (pass1 != pass2)
-    {
-        ui -> password_stat -> setVisible(true);
-        ui -> password_stat -> setText("Passwords Doesnt Match");
-    }
-
-    if ((username1 == " ") || (pass1 == " ") || (pass2 == " ") || (str == " ") || (f == " ") || (r == " "))
+    while((username1.isEmpty()) || (pass1.isEmpty()) || (pass2.isEmpty()) || (year.isEmpty()) || (month.isEmpty()) || (day.isEmpty())||(!acctype) || (!genre) || (!gender))
     {
         ui ->register_status -> setVisible(true);
         ui -> register_status -> setText("Missing Parameters, Please Fill Them in");
-
     }
+    for (int i = 0; i < 100 ; i++)
+    {
+        if (username1 == usernames[i])
+        {
+            ui -> username_stat -> setVisible(true);
+            ui -> username_stat -> setText("Username already exists");
 
+        }
 
+        else
+        {
+            if (pass1 != pass2)
+            {
+                ui -> password_stat -> setVisible(true);
+                ui -> password_stat -> setText("Passwords Doesnt Match");
+            }
+            else
+            {
+                for(int i=usersCount+1;i<100;i++)
+                {
+                    usernames[i]=username1;
+                    passwords[i]=pass1;
+                    ages[i]=2024-year.toInt();
+                    usersCount++;
+                }
 
+            }
+
+        }
+    }
 }
 
