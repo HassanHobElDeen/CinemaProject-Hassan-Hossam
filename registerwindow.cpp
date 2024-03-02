96681eb7b68271a250ec8f8d616dbea2cd904b49
hossamsherif <hossamsherif@aucegypt.edu>
2024-03-02 22:48:42 +0200

    variables

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -16,3 +16,33 @@ RegisterWindow::~RegisterWindow()
 {
     delete ui;
 }
+
+void RegisterWindow::on_reg_reg_clicked()
+{
+    QString str=ui -> month ->currentText();
+    QString f=ui->day->text();
+    QString r=ui->year->text();
+    QString x =( ui->register_username ->text());
+    QString y=(ui->register_pass->text());
+    QString z=(ui->register_pass_2->text());
+    int month = str.toInt();
+    int day=f.toInt();
+    int year=r.toInt();
+    bool acctype=ui->groupBox_acctype->isChecked();
+    bool genre=ui->genre->isChecked();
+    bool gender = ui ->groupBox_gender->isChecked();
+    if(acctype || genre || gender)
+    {
+
+    }
+    else
+    {
+        ui -> register_status ->setVisible(true);
+        ui->register_status->setText("ks");
+    }
+
+
+
+
+}
+
